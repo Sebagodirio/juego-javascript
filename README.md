81b171b1686ab284aad89744a6ed657818fe523a
SebaGodirio <sebastianlgodirio@gmail.com>
2020-09-16 17:34:23 -0300

    Finish game

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # juego-javascript
+
+##Juego de logica para curso de js Avanzado
+
+Por consola se pueden ver los arrays que uno va acertando, como una ayuda a la hora de jugar.
--- app.js
+++ app.js
@@ -1,9 +1,55 @@
+/* 
+ *Global variables
+ */
+
+var myArray = []
+var numbers = []
+var form = document.querySelector('#form')
+var tries = 0
+
+
+/* 
+ *Events
+ */
+var reiniciar = document.querySelector('#again')
+reiniciar.addEventListener('click', (e) => {
+    e.preventDefault()
+    var content = document.querySelector('#content')
+    var winner = document.querySelector('.winner')
+    removeChilds(content)
+    removeValues()
+    var endgame = document.createElement('div')
+
+
+    if (!winner) {
+        endgame.classList = "alert alert-danger small"
+        endgame.textContent = `Los numeros eran ${numbers[0]} ${numbers[1]} ${numbers[2]} y ${numbers[3]} en ese orden. Intentos: ${tries}`
+        content.appendChild(endgame)
+        setTimeout(() => {
+            content.removeChild(endgame)
+        }, 4000)
+    }
+    tries = 0
+    numbers = arrayGenerator()
+
+})
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    tries++
+    if (validate()) {
+        match()
+    }
+
+
+})
+
+/* 
+ * Functions
+ */
 var random = () => {
     return Math.floor(Math.random() * (11 - 0) + 0)
 }
-
-var numbers = []
-
 
 var numberExists = (num) => {
     var flag = false
@@ -15,6 +61,8 @@ var numberExists = (num) => {
 }
 
 var arrayGenerator = () => {
+
+    numbers = []
     for (var i = 0; i < 4; i++) {
         var num = random()
         while (numberExists(num) === true) {
@@ -24,21 +72,12 @@ var arrayGenerator = () => {
         numbers.push(num)
     }
 
+
     return numbers
 }
+numbers = arrayGenerator()
 
-
-
-
-var myArray = []
-
-var form = document.querySelector('#form')
-arrayGenerator()
-console.log(numbers)
-
-form.addEventListener('submit', (e) => {
-    e.preventDefault()
-
+var match = () => {
     var myArray = []
 
     myArray.push(Number(form[0].value))
@@ -59,29 +98,12 @@ form.addEventListener('submit', (e) => {
             wrong++
         }
     }
-    console.log(good, regular, wrong, myArray)
+    console.log(myArray)
+
     results(good, regular, wrong)
-
-})
-
-function removeChilds(content) {
-    var good = document.querySelector('.alert-primary')
-    var regular = document.querySelector('.alert-warning')
-    var wrong = document.querySelector('.alert-danger')
-    if (good) {
-        content.removeChild(good)
-    }
-    if (regular) {
-        content.removeChild(regular)
-    }
-    if (wrong) {
-        content.removeChild(wrong)
-    }
-
-
 }
 
-function results(good, regular, wrong) {
+var results = (good, regular, wrong) => {
 
     var content = document.querySelector('#content')
 
@@ -108,5 +130,73 @@ function results(good, regular, wrong) {
         content.appendChild(wrongEl)
     }
 
+    if (good == 4) {
+        goodEl.textContent = `Felicitaciones, ganaste! Intentos: ${tries}`
+        goodEl.classList.add('winner')
 
+    }
+}
+
+var removeChilds = (content) => {
+    var good = document.querySelector('.alert-primary')
+    var regular = document.querySelector('.alert-warning')
+    var wrong = document.querySelector('.alert-danger')
+    if (good) {
+        content.removeChild(good)
+    }
+    if (regular) {
+        content.removeChild(regular)
+    }
+    if (wrong) {
+        content.removeChild(wrong)
+    }
+
+
+}
+
+var removeValues = () => {
+    form[0].value = ""
+    form[1].value = ""
+    form[2].value = ""
+    form[3].value = ""
+}
+
+var validate = () => {
+    var regex = /[0-9]+/
+
+    var num1 = document.querySelector("input[name=num1]")
+    var num2 = document.querySelector("input[name=num2]")
+    var num3 = document.querySelector("input[name=num3]")
+    var num4 = document.querySelector("input[name=num4]")
+
+    num1.classList.remove('is-invalid')
+    num2.classList.remove('is-invalid')
+    num3.classList.remove('is-invalid')
+    num4.classList.remove('is-invalid')
+    var bool = true
+
+
+    if (!num1.value.match(regex)) {
+        num1.classList.add('is-invalid')
+        bool = false
+    }
+
+    if (!num2.value.match(regex)) {
+        num2.classList.add('is-invalid')
+        bool = false
+    }
+
+    if (!num3.value.match(regex)) {
+        num3.classList.add('is-invalid')
+        bool = false
+    }
+
+    if (!num4.value.match(regex)) {
+        num4.classList.add('is-invalid')
+        bool = false
+    }
+
+
+
+    return bool
 }
